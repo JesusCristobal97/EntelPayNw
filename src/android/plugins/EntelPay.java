08492bcfa642a5040fc1c9429713f82687bf4476
Dcristobalr <daniel.cristobal@entel.pe>
2019-05-27 14:37:28 -0500

    jd2

--- src/android/plugins/EntelPay.java
+++ src/android/plugins/EntelPay.java
@@ -1,7 +1,10 @@
 package plugins;
 
+import android.util.Log;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,17 +15,27 @@ public class EntelPay extends CordovaPlugin {
 
     @Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        Log.e("Ingreso ",action);
+        Log.e("Ingreso",action);
+
         if(action.equals("encriptar")) {
-            
-            String dato = args.getString(0)+"one";
-            
-            JSONObject result = new JSONObject();
-		    result.put("result", dato);
 
-            callbackContext.success(result.toString());
+            try{
+                String dato = args.getString(0)+"one";
+                    
+                    JSONObject result = new JSONObject();
+                    result.put("result", dato);
 
-            return true;
+                    callbackContext.success(result.toString());
+
+                    return true;
+
+            }
+            catch(Exception e){
+                callbackContext.error(e.getMessage()));
+                return false;
+            }
+            
+            
         }
         if(action.equals("desencriptar")) {
             return true;
